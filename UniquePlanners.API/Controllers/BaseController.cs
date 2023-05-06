3ce244ccc670aeb67431ea1eeb1e21b7ac47be8e
Deniel Knezevic <deniel.knezevic@edu.fit.ba>
2023-05-06 19:04:18 +0200

    Add base and CRUD controller

---
+++ UniquePlanners.API/Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UniquePlanners.Application.Services.Base;
+
+namespace UniquePlanners.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController<T,TSearch> : ControllerBase where T : class where TSearch : class 
+    {
+        public readonly IBaseService<T,TSearch> _service;
+        public BaseController(IBaseService<T, TSearch> service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public virtual async Task<ActionResult<IEnumerable<T>>> GetAll(TSearch search)
+        {
+            return Ok(await _service.GetAll(search));
+        }
+
+        [HttpGet("{id}")]
+        public virtual async Task<ActionResult<T>> GetById(TSearch search, object id)
+        {
+            return Ok(await _service.GetById(id));
+        }
+    }
+}
---
+++ UniquePlanners.API/Controllers/CRUDController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UniquePlanners.Application.Services.Base;
+
+namespace UniquePlanners.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CRUDController<T,TSearch,TInsertRequest,TUpdateRequest> : BaseController<T,TSearch>
+        where T : class where TSearch : class where TInsertRequest : class where TUpdateRequest : class
+    {
+        public CRUDController(ICRUDService<T,TSearch,TInsertRequest,TUpdateRequest> service):base(service)
+        {
+
+        }
+
+        [HttpPost]
+        public virtual async Task<ActionResult<T>> Post([FromBody]TInsertRequest entity)
+        {
+            return Ok(await ((ICRUDService<T, TSearch, TInsertRequest, TUpdateRequest>)_service).Insert(entity));
+        }
+
+        [HttpPut("{id}")]
+        public virtual async Task<ActionResult<T>> Put([FromBody]TUpdateRequest entity, object id)
+        {
+            return Ok(await ((ICRUDService<T, TSearch, TInsertRequest, TUpdateRequest>)_service).Update(entity, id));
+        }
+    }
+}
